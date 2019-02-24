19fa6eee3a215f26dc3a2143c391a1b3723d73d5
Kartikay <shandilkartikay@gmail.com>
2019-02-24 19:43:30 +0530

    completed basic fetch links

--- src/new_populate_links.py
+++ src/new_populate_links.py
@@ -8,8 +8,6 @@ import platform
 import time
 import sys
 
-def get_token(anime_url):
-    return animeurl.split("/")[-1]
 #getting driver
 filepath=os.path.realpath(__file__).replace("/src/new_populate_links.py","")
 driverpath=filepath+"/driver/chromedriver"
@@ -22,7 +20,7 @@ options.add_argument("--disable-notifications")
 
 #visiting the website
 browser = webdriver.Chrome(driverpath,chrome_options=options)
-browser.get('https://www.9anime.to')
+#browser.get('https://www.9anime.to')
 
 #getting download details
 myfile=open(filepath+"/res/details.dat")
@@ -30,30 +28,72 @@ animeurl=myfile.readline()
 start=int(myfile.readline())
 end=int(myfile.readline())
 qual=int(myfile.readline())
-token=get_token(animeurl)
 
-#visiting the website
-browser.get(animeurl)
+def get_token(anime_url):
+    return animeurl.split("/")[-1][:-1]
 
-#checking if RapidVideo server exists for the given anime
-server=browser.find_elements_by_class_name("tab")
-exists=False
-for i in server:
-    if i.text=='RapidVideo':
-        i.click()
-        exists=True
-        break
-if not exists:
-    browser.quit()
-    sys.exit("The server for RapidVideo does not exist,\
-    \nWe cannot download this anime")
+def setup():
+    token='[data-id="'+get_token(animeurl)+'"]'
+    print(token)
+    todwnld=[x for x in range(start,end+1)]
+    #visiting the website
+    browser.get(animeurl)
 
-#getting all the episodes
-ep_heads=browser.find_elements_by_css_selector(".episodes.range.active")
-#selecting links from RapidVideo only
-rapid_ephead=ep_heads[0]
-for ep_head in ep_heads:
-    print(ep_head.find_element_by_tag_name("li").get_attribute("data-id"))
-print(rapid_ephead.get_attribute("innerHTML"))
+    #checking if RapidVideo server exists for the given anime
+    server=browser.find_elements_by_class_name("tab")
+    exists=False
+    for i in server:
+        if i.text=='RapidVideo':
+            i.click()
+            exists=True
+            break
+    if not exists:
+        browser.quit()
+        sys.exit("The server for RapidVideo does not exist,\
+        \nWe cannot download this anime")
+
+    #getting all the episodes
+    ep_heads=browser.find_elements_by_css_selector(".episodes.range.active")
+    #selecting links from RapidVideo only
+    rapid_ephead=ep_heads[0]
+    for ep_head in ep_heads:
+        if(ep_head.find_elements_by_css_selector(token)):
+            rapid_ephead=ep_head
+            break
+
+    #get list of all episodes
+    eplist=rapid_ephead.find_elements_by_tag_name('li')
+    d=dict()
+    for li in eplist:
+        db=li.find_element_by_tag_name('a').get_attribute('data-base')
+        link=li.find_element_by_tag_name('a').get_attribute('href')
+        d[db]=link
+
+    #set the episodes to be downloaded
+    todwnldlist=dict()
+    for x in todwnld:
+        no=str(x)
+        if no in d.keys():
+            todwnldlist[no]=d[no]
+        else:
+            print("Episode",no,"not in the list of available episodes")
+    print(todwnldlist)
+    get_actual_links(todwnldlist,'480p')
+#get actual download links
+def get_actual_links(ep_links,quality):
+
+    for ep_no in ep_links.keys():
+        browser.get(ep_links[ep_no])
+        click_here=browser.find_element_by_class_name("cover")
+        click_here.click()
+        #wait for iframe to download
+        time.sleep(4)
+        #switch to the new loaded iframe
+        iframe=browser.find_elements_by_tag_name("iframe")[0]
+        browser.switch_to_frame(iframe)
+        # TODO: Find better alternate
+        videoplayer=browser.find_elements_by_id('home-video')
+
+setup()
 #quit after visit
 browser.quit()
